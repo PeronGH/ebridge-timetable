a0a64536cd665999a98fd8db65062198ff9f568c
peron <peron_ap@icloud.com>
2022-09-07 21:40:39 +0800

    update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,15 @@
-export { parseTimetable } from './parser.ts';
-export { genCalendar } from './main.ts';
-export { DOMParser } from './deps.ts';
+import { parseTimetable } from './parser.ts';
+import { genCalendar } from './main.ts';
+import { DOMParser } from './deps.ts';
+
+export { parseTimetable, genCalendar, DOMParser };
+
+export function stringToIcs(text: string) {
+  const document = new DOMParser().parseFromString(text, 'text/html')!;
+
+  const lessons = parseTimetable(document);
+
+  const calendar = genCalendar(lessons);
+
+  return calendar.toString();
+}
