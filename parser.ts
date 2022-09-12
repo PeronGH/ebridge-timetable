f85cf6cc04f4d8bfe91aaf1ee5d985760474d321
peron <peron_ap@icloud.com>
2022-09-12 20:06:02 +0800

    fix bug for certain ebridge page

--- parser.ts
+++ parser.ts
@@ -2,6 +2,8 @@ import { HTMLDocument, Day } from './deps.ts';
 
 export const DAYS: Day[] = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'];
 
+const customDays: Day[] = [];
+
 export function parseTimetable(document: HTMLDocument) {
   const lessonDict: { [title: string]: Lesson } = {};
 
@@ -11,7 +13,13 @@ export function parseTimetable(document: HTMLDocument) {
   const rows = tbody.children;
 
   for (let row_i = 0; row_i < rows.length; ++row_i) {
-    if (row_i === 0) continue;
+    if (row_i === 0) {
+      // First row (days of the week)
+      for (const cell of rows[row_i].children) {
+        const day = cell.textContent.trim().slice(0, 2).toUpperCase() as Day;
+        customDays.push(day);
+      }
+    }
     // Walk through every row
     const row = rows[row_i];
 
@@ -21,7 +29,7 @@ export function parseTimetable(document: HTMLDocument) {
     const cells = row.children;
 
     for (let col_i = 0; col_i < cells.length; ++col_i) {
-      const day = DAYS[col_i - 1]!;
+      const day = customDays[col_i]!;
       // Walk through every cell
       const cell = cells[col_i];
 
